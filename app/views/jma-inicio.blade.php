99583018d57fdfd8b68a3158ae1fe884ff630e02
eugeMolinero <euge_molinero@yahoo.com.ar>
2015-02-18 13:57:40 -0300

    Página PERFILES y arreglos
    Arreglos en estilos, contacto + links y ampliación en la HOME

--- app/views/jma-inicio.blade.php
+++ app/views/jma-inicio.blade.php
@@ -12,25 +12,31 @@
         <h1>Somos la Empresa Argentina Líder en fabricación y comercialización de Perfiles de Acero Galvanizado, desarrollando todas las variantes que demandan el mercado interno y externo:</h1>
     </div>
     <div class="colFotosHome">
-        <h2>PERFILES DESMONTABLES</h2>
-        <div class="bordeVerdeLateral">
-            <img src="{{URL::to('images/perfiles-desmontables.jpg')}}" alt="Perfiles Desmontables">
-            <p>Sistema de suspensión para cielorrasos desmontables</p>
-        </div>
+        <a href="{{URL::to('menu/perfiles-desmontables')}}">
+            <h2>PERFILES DESMONTABLES</h2>
+            <div class="bordeVerdeLateral">
+                <img class="ampliar" src="{{URL::to('images/perfiles-desmontables.jpg')}}" alt="Perfiles Desmontables">
+                <p>Sistema de suspensión para cielorrasos desmontables</p>
+            </div>
+        </a>
     </div>
     <div class="colFotosHome">
-        <h2>DRYWALL</h2>
-        <div class="bordeVerdeLateral">
-            <img src="{{URL::to('images/drywall.jpg')}}" alt="Perfiles Desmontables">
-            <p>Perfiles y accesorios para construcción en seco</p>
-        </div>
+        <a href="{{URL::to('menu/drywall')}}">
+            <h2>DRYWALL</h2>
+            <div class="bordeVerdeLateral">
+                <img class="ampliar" src="{{URL::to('images/drywall.jpg')}}" alt="Perfiles Desmontables">
+                <p>Perfiles y accesorios para construcción en seco</p>
+            </div>
+        </a>
     </div>
     <div class="colFotosHome">
-        <h2>STEEL FRAMING</h2>
-        <div class="bordeVerdeLateral">
-            <img src="{{URL::to('images/steel-framing.jpg')}}" alt="Perfiles Desmontables">
-            <p>Perfiles <br>estructurales</p>
-        </div>
+        <a href="{{URL::to('menu/steel-framing')}}">
+            <h2>STEEL FRAMING</h2>
+            <div class="bordeVerdeLateral">
+                <img class="ampliar" src="{{URL::to('images/steel-framing.jpg')}}" alt="Perfiles Desmontables">
+                <p>Perfiles <br>estructurales</p>
+            </div>
+        </a>
     </div>
     <div class="clear"></div>
 </section>
